8c48d58832a3fdefdfd918efc29e374eb3b78528
Jatin <ym.jatin@gmail.com>
2025-04-09 17:23:05 +0530

    chore: add typography

--- app/utils/typography.ts
+++ app/utils/typography.ts
@@ -1,4 +1,21 @@
 import { defineTextStyles } from '@pandacss/dev';
 
 //if you modify textStyles you might want to run the 'yarn prepare' command for panda to generate based on the changes.
-export const textStyles = defineTextStyles({});
+export const textStyles = defineTextStyles({
+    title: {
+        value: {
+            fontSize: { base: '2xl', lg: '4xl' },
+            fontWeight: 'semibold'
+        }
+    },
+    'sub-title': {
+        value: {
+            fontSize: 'xl'
+        }
+    },
+    body: {
+        value: {
+            fontSize: 'md'
+        }
+    }
+});
