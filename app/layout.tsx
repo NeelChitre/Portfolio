59889f82e8e934dcff10e55066a59a108e155826
Jatin <ym.jatin@gmail.com>
2025-04-09 23:24:05 +0530

    chore: add meta info

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,38 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-    title: 'Neel Chitre',
-    description: 'Portfolio Website'
+    title: 'Neel Chitre | Game Developer',
+    description:
+        'Portfolio website of Neel Chitre, a passionate game developer with experience in Unity, Unreal Engine.',
+    metadataBase: new URL('https://neelchitre.vercel.app'),
+    openGraph: {
+        title: 'Neel Chitre | Game Developer',
+        description:
+            'Explore the portfolio of Neel Chitre, a skilled game developer specializing in creating immersive experiences with Unity and Unreal Engine.',
+        url: 'https://neelchitre.vercel.app',
+        siteName: 'Neel Chitre Portfolio',
+        images: [
+            {
+                url: 'https://neelchitre.vercel.app/og_image.png',
+                width: 800,
+                height: 800,
+                alt: 'Neel Chitre Profile Image'
+            }
+        ],
+        type: 'website'
+    },
+    keywords: [
+        'Neel Chitre',
+        'Game Developer',
+        'Unity',
+        'Unreal Engine',
+        'Indie Games',
+        'Game Design'
+    ],
+    robots: {
+        index: true,
+        follow: true
+    }
 };
 
 export default function RootLayout({
