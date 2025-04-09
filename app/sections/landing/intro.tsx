226bd1f7f19840c8f4bc2e8f5a2709e07fb60951
Jatin <ym.jatin@gmail.com>
2025-04-09 17:29:42 +0530

    chore: add intro section content

--- app/sections/landing/intro.tsx
+++ app/sections/landing/intro.tsx
@@ -1,12 +1,46 @@
-import { Section } from '@/app/components';
+import { Button, Section, Text } from '@/app/components';
+import { Flex } from 'styled-system/jsx';
 import { FC } from 'react';
+import { contactInfo } from '@/app/data';
+import Link from 'next/link';
+import { GithubIcon, LinkedinIcon } from 'lucide-react';
 
 export interface IntroProps {}
 
 export const Intro: FC<IntroProps> = () => {
     return (
         <Section id="intro" variant="standard">
-            intro
+            <Flex direction={'column'} gap={'3'} alignItems={'center'}>
+                <Text as={'h3'} fontSize={{ base: 'md' }}>
+                    Hello I'm
+                </Text>
+                <Text as={'h1'} fontSize={{ base: '2xl', lg: '4xl' }}>
+                    {contactInfo.name}
+                </Text>
+                <Text as={'h2'} fontSize={{ base: 'xl' }}>
+                    {contactInfo.role}
+                </Text>
+            </Flex>
+            <Flex my={'8'} gap={'8'}>
+                <a href={`${contactInfo.resumePath}`} download>
+                    <Button variant={'solid'} size={'lg'}>
+                        Download CV
+                    </Button>
+                </a>
+                <Link href="/#contact">
+                    <Button variant={'outline'} size={'lg'}>
+                        Contact Info
+                    </Button>
+                </Link>
+            </Flex>
+            <Flex my={'8'} gap={'8'}>
+                <Link href={contactInfo.githubProfile}>
+                    <GithubIcon />
+                </Link>
+                <Link href={contactInfo.linkedIn}>
+                    <LinkedinIcon />
+                </Link>
+            </Flex>
         </Section>
     );
 };
