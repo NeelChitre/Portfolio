521d941ea2dfae8cbee9a78be4cfbc35807d1a44
Jatin <ym.jatin@gmail.com>
2025-04-09 17:27:49 +0530

    chore: apply typography on logo

--- app/components/header/logo.tsx
+++ app/components/header/logo.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 import { FC } from 'react';
+import { Text } from '../ui';
 
 export interface HeaderLogoProps {}
 
 export const HeaderLogo: FC<HeaderLogoProps> = () => {
-    return <Link href="/#intro">Neel Chitre</Link>;
+    return (
+        <Link href="/#intro">
+            <Text textStyle={'logo'}>Neel Chitre</Text>
+        </Link>
+    );
 };
