4316e5f38fb25734fa0a599a2db5eaaa7cbc7e60
Jatin <ym.jatin@gmail.com>
2025-04-09 17:23:27 +0530

    chore: make all contact fields required

--- app/utils/contact.type.ts
+++ app/utils/contact.type.ts
@@ -2,8 +2,8 @@ export interface IContact {
     name: string;
     role: string;
     email: string;
-    mobile?: string;
-    resumePath?: string;
-    linkedIn?: string;
-    githubProfile?: string;
+    mobile: string;
+    resumePath: string;
+    linkedIn: string;
+    githubProfile: string;
 }
