cf9efe4362b0a19597895fde41a31601c6b574b5
Jatin <ym.jatin@gmail.com>
2025-04-09 17:24:26 +0530

    chore: add path to resume in contact info

--- app/data/contact.ts
+++ app/data/contact.ts
@@ -3,6 +3,7 @@ import { IContact } from '../utils';
 export const contactInfo: IContact = {
     name: 'Neel Chitre',
     role: 'Game Developer',
+    resumePath: '/cv/neel_cv.pdf',
     email: 'neel.chitre507@gmail.com',
     mobile: '+91 9511819401',
     githubProfile: 'https://github.com/NeelChitre',
