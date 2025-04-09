6cc622aeb7fcbe0664f4c3ea06ad0e6a2207acd5
Jatin <ym.jatin@gmail.com>
2025-04-09 17:28:13 +0530

    chore: add footer content

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,22 @@
+import { css } from 'styled-system/css';
+import { Container, Flex, FlexProps } from 'styled-system/jsx';
 import { FC } from 'react';
+import { HeaderLinks } from '../header/header-links';
+import { Text } from '../ui';
 
-export interface FooterProps {}
+export interface FooterProps extends FlexProps {}
 
-export const Footer: FC<FooterProps> = () => {
-    return <>Footer</>;
+export const Footer: FC<FooterProps> = (props) => {
+    return (
+        <Flex alignItems={'center'} w={'full'} my={'8'} gap={'10'} {...props}>
+            <Container>
+                <Flex alignItems={'center'} direction={'column'} gap={'10'} {...props}>
+                    <HeaderLinks gap={'4'} />
+                    <Text as={'p'} textAlign={'center'} textStyle={'body'}>
+                        Copyright &copy; 2025 Neel Chitre. All Rights Reserved.
+                    </Text>
+                </Flex>
+            </Container>
+        </Flex>
+    );
 };
