96804e7fef543ddad341dfed82d2e760cd0968a6
Jatin <ym.jatin@gmail.com>
2025-04-10 17:46:03 +0530

    chore: add carousel component

---
+++ app/components/carousel/carousel.tsx
@@ -0,0 +1,62 @@
+'use client';
+import React from 'react';
+import { EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import Fade from 'embla-carousel-fade';
+import { NextButton, PrevButton, usePrevNextButtons } from './carousel-arrows';
+import { DotButton, useDotButton } from './carousel-dot-button';
+import Image from 'next/image';
+
+type PropType = {
+    slides: string[];
+    options?: EmblaOptionsType;
+};
+
+export const EmblaCarousel: React.FC<PropType> = (props) => {
+    const { slides, options } = props;
+    const [emblaRef, emblaApi] = useEmblaCarousel(options, [Fade()]);
+
+    const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
+
+    const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } =
+        usePrevNextButtons(emblaApi);
+
+    return (
+        <div className="embla">
+            <div className="embla__viewport" ref={emblaRef}>
+                <div className="embla__container">
+                    {slides.map((slide, index) => (
+                        <div className="embla__slide" key={index}>
+                            <Image
+                                className="embla__slide__img"
+                                src={slide}
+                                alt="Game preview image"
+                                width={500}
+                                height={400}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="embla__controls">
+                <div className="embla__buttons">
+                    <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+                    <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+                </div>
+
+                <div className="embla__dots">
+                    {scrollSnaps.map((_: any, index: number) => (
+                        <DotButton
+                            key={index}
+                            onClick={() => onDotButtonClick(index)}
+                            className={'embla__dot'.concat(
+                                index === selectedIndex ? ' embla__dot--selected' : ''
+                            )}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,6 @@
 export * from './box';
 export * from './cards';
+export * from './carousel';
 export * from './footer';
 export * from './header';
 export * from './section';
