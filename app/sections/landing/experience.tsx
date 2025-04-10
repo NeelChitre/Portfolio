d98be0022771cb84345e15e40bb2be49416a6f9d
Jatin <ym.jatin@gmail.com>
2025-04-10 17:47:27 +0530

    chore: add experience section content

--- app/sections/landing/experience.tsx
+++ app/sections/landing/experience.tsx
@@ -1,12 +1,27 @@
-import { Section } from '@/app/components';
+import { Section, Text } from '@/app/components';
+import { ExperienceCard } from '@/app/components/cards/experience-card';
+import { experience } from '@/app/data/experience';
 import { FC } from 'react';
+import { Flex } from 'styled-system/jsx';
 
 export interface ExperienceProps {}
 
 export const Experience: FC<ExperienceProps> = () => {
     return (
         <Section id="experience" variant="standard">
-            experience
+            <Flex direction={'column'} alignItems={'center'} gap={'2'}>
+                <Text as={'h3'} textStyle={'body'}>
+                    What I've been doing
+                </Text>
+                <Text as={'h1'} textStyle={'title'}>
+                    Expericence
+                </Text>
+            </Flex>
+            <Flex justifyContent={'center'} flexWrap={'wrap'} gap={'4'} my={'12'}>
+                {experience.map((props, index) => (
+                    <ExperienceCard key={index} {...props} />
+                ))}
+            </Flex>
         </Section>
     );
 };
