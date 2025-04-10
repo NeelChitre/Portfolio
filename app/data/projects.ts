9415358c466e35034883451ed1b97132fc87cbfb
Jatin <ym.jatin@gmail.com>
2025-04-10 17:48:30 +0530

    chore: update project data with dummy data

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,22 +3,61 @@ import { IProject } from '../utils';
 export const projectsData: IProject[] = [
     {
         name: 'Kitchen Chaos',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         slug: 'kitchen_chaos',
-        githubLink: 'https://github.com/NeelChitre/KitchenChaos.git'
+        githubLink: 'https://github.com/NeelChitre/KitchenChaos.git',
+        playableLink: 'https://neelchitre.itch.io/kitchen-chaos',
+        thumbnail: 'https://picsum.photos/500/400',
+        pictures: [
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400'
+        ]
     },
     {
         name: 'This is Blast ( Recreation )',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         slug: 'this_is_blast',
-        githubLink: 'https://github.com/NeelChitre/This-is-Blast-Recreation-.git'
+        githubLink: 'https://github.com/NeelChitre/This-is-Blast-Recreation-.git',
+        playableLink: 'https://youtube.com/shorts/3FTUrMPcFVQ?feature=share',
+        thumbnail: 'https://picsum.photos/500/400',
+        pictures: [
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400'
+        ]
     },
     {
         name: 'CBB',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         slug: 'cbb',
-        githubLink: 'https://github.com/NeelChitre/Chitale-Bakarwadi-Blitz.git'
+        githubLink: 'https://github.com/NeelChitre/Chitale-Bakarwadi-Blitz.git',
+        thumbnail: 'https://picsum.photos/500/400',
+        pictures: [
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400'
+        ]
     },
     {
         name: 'WMC',
+        description:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         slug: 'wmc',
-        githubLink: 'https://github.com/NeelChitre/Where-s-My-Coin.git'
+        githubLink: 'https://github.com/NeelChitre/Where-s-My-Coin.git',
+        playableLink: 'https://neelchitre.itch.io/wheres-my-coin',
+        thumbnail: 'https://picsum.photos/500/400',
+        pictures: [
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400',
+            'https://picsum.photos/500/400'
+        ]
     }
 ];
