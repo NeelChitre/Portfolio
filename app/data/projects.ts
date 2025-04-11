1d9597ffa58684db6665094bcfd15d5c857ba1cc
Jatin <ym.jatin@gmail.com>
2025-04-11 13:19:45 +0530

    chore: add project description

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -3,8 +3,11 @@ import { IProject } from '../utils';
 export const projectsData: IProject[] = [
     {
         name: 'Kitchen Chaos',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: [
+            'Kitchen Chaos is a fast-paced, single-player cooking game that drops players into the middle of a high-pressure kitchen where multitasking is everything. You’ll chop ingredients, cook dishes, plate meals and serve them, juggling multiple tasks at once to keep the orders flowing and the customers happy.',
+            'Designed to test your speed, focus, and coordination, the game keeps you constantly on your toes as the kitchen gets busier and more chaotic with each passing moment. From sizzling pans to timers ticking down, every action counts in this race against the clock.',
+            'With its vibrant visuals, intuitive controls, and nonstop energy, Kitchen Chaos delivers a fun and frantic cooking experience that captures the intensity of a one-chef kitchen.'
+        ],
         slug: 'kitchen_chaos',
         githubLink: 'https://github.com/NeelChitre/KitchenChaos.git',
         playableLink: 'https://neelchitre.itch.io/kitchen-chaos',
@@ -14,13 +17,16 @@ export const projectsData: IProject[] = [
             '/kitchen_chaos/kc_1.png',
             '/kitchen_chaos/kc_2.png',
             '/kitchen_chaos/kc_3.png',
-            '/kitchen_chaos/kc_4.png',
+            '/kitchen_chaos/kc_4.png'
         ]
     },
     {
         name: 'This is Blast! ( Recreation )',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: [
+            'This is Blast! (Recreation) is a strategy-based puzzle game where the objective is to clear block structures using color-matched shooters. Players must carefully analyze each level and choose the correct shooter that matches the color of the blocks in the puzzle. Timing and strategy are key to clearing all blocks efficiently.',
+            'Built in Unity, this project is a faithful recreation of the first 5 levels of the original This is Blast! mobile game. While simple in concept, the game challenges players to think ahead, plan their moves, and clear each stage with maximum efficiency.',
+            "Whether you're into thoughtful puzzle-solving or just enjoy color-based gameplay mechanics, This is Blast! (Recreation) offers a compact and engaging experience with a strategic twist."
+        ],
         slug: 'this_is_blast',
         githubLink: 'https://github.com/NeelChitre/This-is-Blast-Recreation-.git',
         playableLink: 'https://youtube.com/shorts/3FTUrMPcFVQ?feature=share',
@@ -30,13 +36,16 @@ export const projectsData: IProject[] = [
             '/this_is_blast/blast_2.png',
             '/this_is_blast/blast_3.png',
             '/this_is_blast/blast_4.png',
-            '/this_is_blast/blast_5.png',
+            '/this_is_blast/blast_5.png'
         ]
     },
     {
         name: 'Chitale Bakarwadi Blitz',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        description: [
+            'Chitale Bakarwadi Blitz is a colorful 3D endless runner set in the vibrant streets of Pune, where tradition meets adrenaline. Players dash through iconic cityscapes, dodging obstacles and collecting delicious bakarwadis, all while aiming for the highest score.',
+            'Inspired by local culture and the beloved Chitale Bandhu snack, the game combines fast-paced gameplay with a unique regional twist. Smooth controls, catchy visuals, and an ever-accelerating pace make each run feel fresh, fun, and delightfully chaotic.',
+            "Whether you're chasing high scores or just craving some virtual bakarwadi, Chitale Bakarwadi Blitz brings Pune’s flavor-packed energy straight to your screen."
+        ],
         slug: 'cbb',
         githubLink: 'https://github.com/NeelChitre/Chitale-Bakarwadi-Blitz.git',
         playableLink: 'https://youtube.com/shorts/k5pwdPQm-1I?feature=share',
@@ -46,14 +55,16 @@ export const projectsData: IProject[] = [
             '/cbb/cbb_character.png',
             '/cbb/cbb_start.png',
             '/cbb/cbb_slide.png',
-            '/cbb/cbb_jump.png',
-            
+            '/cbb/cbb_jump.png'
         ]
     },
     {
-        name: 'Where\'s My Coin?',
-        description:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        name: "Where's My Coin?",
+        description: [
+            'Where’s My Coin? is a 3D third-person multiplayer game set in a lively city filled with hidden secrets and challenges. Players are dropped into an open urban environment with one simple goal—find all 25 hidden coins and reach the finish line on the other side of the city. But the journey isn’t as easy as it sounds. The coins are cleverly tucked away in alleys, rooftops, and corners that test your exploration and observation skills.',
+            'The game offers two distinct modes. Easy Mode lets players take their time, explore at their own pace, and enjoy the thrill of discovery without pressure. Hard Mode, on the other hand, turns up the intensity with a ticking timer, fail to collect all the coins before time runs out, and it’s game over.',
+            'Designed for both solo play and friendly competition, Where’s My Coin? brings together exploration, time-based strategy, and a bit of playful chaos in a city-wide scavenger hunt.'
+        ],
         slug: 'wmc',
         githubLink: 'https://github.com/NeelChitre/Where-s-My-Coin.git',
         playableLink: 'https://neelchitre.itch.io/wheres-my-coin',
@@ -63,7 +74,7 @@ export const projectsData: IProject[] = [
             '/wmc/wmc_1.png',
             '/wmc/wmc_2.png',
             '/wmc/wmc_3.png',
-            '/wmc/wmc_4.png',
+            '/wmc/wmc_4.png'
         ]
     }
 ];
