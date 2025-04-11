9691498d36f28609b3613d6f6feaa02b987873f5
Jatin <ym.jatin@gmail.com>
2025-04-11 13:56:56 +0530

    chore: make all cards same in height

--- app/components/cards/project-card.tsx
+++ app/components/cards/project-card.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
 import { css } from 'styled-system/css';
-import { Flex } from 'styled-system/jsx';
+import { Box, Flex } from 'styled-system/jsx';
 import { Button, Text } from '../ui';
 
 export interface ProjectCardProps extends Partial<IProject> {}
@@ -14,10 +14,12 @@ export interface ProjectCardProps extends Partial<IProject> {}
 export const ProjectCard: FC<ProjectCardProps> = ({ thumbnail, playableLink, name, slug }) => {
     return (
         <Flex
+            gap={'8'}
             alignItems={'center'}
             direction={'column'}
             w={'full'}
             maxW={'md'}
+            h={'md'}
             p={{ base: '4', md: '8' }}
             border={'gray'}
             borderStyle={'solid'}
@@ -27,17 +29,17 @@ export const ProjectCard: FC<ProjectCardProps> = ({ thumbnail, playableLink, nam
                 backgroundColor: 'white.a1'
             }}
         >
-            <Link href={`/projects/${slug}`}>
-                <Image
-                    src={thumbnail ?? '/placeholder-image.jpg'}
-                    alt="project-thumbnail"
-                    className={imageStyles}
-                    width={400}
-                    height={400}
-                />
-                <Text textAlign={'center'} my={'8'}>
-                    {name}
-                </Text>
+            <Link href={`/projects/${slug}`} className={linkStyles}>
+                <Box w={'full'} flexGrow={1}>
+                    <Image
+                        src={thumbnail ?? '/placeholder-image.jpg'}
+                        alt="project-thumbnail"
+                        className={imageStyles}
+                        width={400}
+                        height={400}
+                    />
+                </Box>
+                <Text textAlign={'center'}>{name}</Text>
             </Link>
             <Flex my={'1'} gap={'8'} alignItems={'center'}>
                 {playableLink && (
@@ -61,8 +63,16 @@ export const ProjectCard: FC<ProjectCardProps> = ({ thumbnail, playableLink, nam
     );
 };
 
-const imageStyles = css({
+const linkStyles = css({
+    display: 'flex',
     flexGrow: 1,
+    flexDirection: 'column',
+    gap: '4'
+});
+
+const imageStyles = css({
+    h: 'full',
+    maxH: '64',
     objectFit: 'cover',
     objectPosition: 'center',
     borderRadius: 'xl'
